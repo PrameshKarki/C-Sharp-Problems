a75fb18dfbedb834e3f8f02d6033bc7118ca2a9f
Pramesh Karki <Parameswarkarki@gmail.com>
2021-02-22 14:00:47 +0545

    Display Current Time/

---
+++ Display Current Time/CurrentTime.cs
@@ -0,0 +1,11 @@
+using System;
+namespace CurrentTimeApplication {
+    public class MainClass {
+        static void Main (string[] args) {
+            Console.WriteLine("Current Time:{0}",DateTime.Now);
+            Console.Read ();
+        }
+    }
+}
+
+//Pramesh Karki
